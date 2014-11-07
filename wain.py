8c27e467906131a5c387edac3131b257a982810f
wcmckee <will@artcontrol.me>
2014-11-07 20:53:58 +0000

    code

--- wain.py
+++ wain.py
@@ -51,7 +51,7 @@ import json
 # 5. everything seems fine
 # 
 # Charlie MacDonald sat at his desk steering at the monitor. 
-# 'Alfred Bunnings, who is this guy?', he thought to himself. 'Jones, get over here and check this for me', Charlie yelled out to his Assistent.
+# 'Alfred Bunnings, who is this guy?', he thought to himself. 'Jones, get over here and see this', Charlie yelled out to his Assistent.
 # 'Coming Sir!', a voice sharply replied. 
 # Into Charlies office walked Jones. Tall, 6 foot 3. Dark Brown hair, curly and down to his shoulders. On this day his hair was tied up, usually he left it down but today was special.
 # 'What's up sir', Jones asked, squinting at the monitor. 
@@ -75,7 +75,7 @@ import json
 # The automate doors opened as he approched them. Inside stood three security guards and a metal detotor. 
 # 'Name and reason/person visit', one of the guards asked him. 
 # 'Alfred Bunnings, I'm here for a job interview with Janet Pears'.
-# The guard typed characters into the computer.
+# The guard typed characters into the comput
 # 'Alright, that seems fine sir', the guard replied'. 'Please make your way down the hall and take the second right on your left'.
 # 'Thank you', Afred said as he begain walking down the hallway. 
 # 
@@ -84,6 +84,7 @@ import json
 # The colour scheme changed, colours filled the walls. Character and Landscapes scatted the walls. It was painted with a dark naples yellow, red, and a light blue. The backgrounds were blue and the characters and foreground landscapes were yellow and red. 
 # 
 # Alfred admired the walls as he contined walking down the hallway. At the end of the hallway was a foylor with several desks setup with computers. A woman sat at the further one typing. 
+# The foylar was plainer than the hallway. Art murels still scatted the area, but they were less freuent. The are had a light blue background, with yellow and red character and landscape artwork. 
 # She looked up and smiled at Alfred.
 # 'You must be Alfred', she exampled. 
 # Alfred approched her desk and held out he hand to shake it.
@@ -95,7 +96,7 @@ import json
 # 
 # Twenty years he had worked there. Today he was moving on, had sold his shares of the company.
 # He approched the doors. They opened automatitly. 
-# Three security guards stood there. All with very serious looks on their faces.
+# Three security guards stood there. All with very serious looks on their faces. Unlike the day he walked into the building for a job interview the guards were humanoild shaped robots. The guard looked and sounded like the same security guards on that first day. Under their fake human skin was just metal and wires.  
 # 'Morning fellas', Alfred said as he flashed his ID badge and walked down the corodilol. 
 # The corodor was still completely white, like the first time he entered the building. 
 # He walked down the hallway. Passing the first door on the left, and turning left at the second turn. 
@@ -109,6 +110,8 @@ import json
 # 'Goodmorning Janet', Alfred smiled.
 # Janet rose from her chair.
 # 'Time for a coffee I believe', she said.
+# Janet hadn't aged a day since Alfred met her. She had long straight brunette hair. Light complection skin, with frekles covering her face. She wore skinny horozonal glasses that had a light blue tint. On this day she wore a long silky dress. Her black leather jacket was spread out over the back of her computer chair. 
+# On her desk sat three wireless 22 inch monitors. 
 # 
 # The ground that Janet walked on was rough. Stones cut into her barefeet. The sun blinded her view, she held her arm above her eyes. Attempting to block out the rays. 
 # A figure amurged in the distance.. Squinting she made haste towards the figure. As she drew closer she noticed the figure was a copy of herself. A clone. 
@@ -123,12 +126,47 @@ import json
 # The drone showed three people exiting the truck. Two men and a woman. As they scretched to a halt outside the house of Elizebeth Goodwill, was in fact Janet lived there. It was a setup. The house was trapped so that when they entered - they were never going to leave again. 
 # Thirty years Noel Locksmart had been a prisnor in the house. He will never forget the night. It was just a standard robbery. Locksmart had recieved word of a computer scientice by the name of Elizebeth Goodwill that had developed software that allowed the replaying of events in the past. The system was complex but started with just one script when Goodwill was in her early twentys. It was a login and logout script. The first script was the login script. This created a JSON object that included firstname, lastname, date (04/Nov/2014), time (hr:min), reason, and comment. The logout script added to the JSON object logout date, time, and comment. 
 # 
-# Ali Turbon wislied as he mixed the flour and eggs together. Pandora radio was playing from her bluetooth. George Benson played. 'Onions are ready to go, confirm? Y/n', a device in Turbon's left chefs jacket beeped. Turbon reached for her phone and hit enter. Three claws reached inside the cubord and picked up an onion each. The bench was a woodern bench. The claws dropped the onions on the bench then went back to the cubard to collect more onions. Several more trips back and forward then the claws switched into the next phase - cutting of the onion. The onions were lined up perfectly on the bench. Three rows, five collums. Fifteen onions. Chef knives snapped out of the claws of the device and started to cut up the onions. Each onion was cut in half, then quarters, and finally sliced. The onions were scopped up and droped into a pan of oil to fry. Once the onions were a lovely gold color they were tiped into a slow cooker. 
-# The claws grabbed a orange carrot and dropped it on the bench. The same amount of carrots were used as onions. The knives cut the carrots by a slice lengthways in half, then quarters, and finally slicing. Scopped up by the claws they are dropped in a pan to be fried with oil. Once goldern the carrots are tipped into the slow cooker. Fifteen cups of soft chickpeas are tipped into the pan of oil and fried. They are tipped into the slow cooker. Fifteen medium sized tomaroes travel through the room, being griped by the claw. They are crushed and tipped into the slowcooker.  
+# Ali Turbon wislied as he mixed the Indian spice together. Pandora radio was playing from his bluetooth speaker. It was on the family dinner table. Next to the speaker was a nonbranded laptop running Fedora. KDE was the desktop enviorment. Firefox was open with a IPython Notebook on the screen. This window was on the left side of the monitor. On the right top side of the monitor a television show played. It was   George Benson played. 'Onions are ready to go, confirm? Y/n', a device in Turbon's left chefs jacket beeped. Turbon reached for his phone and hit enter. Three claws reached inside the cubord and picked up an onion each. The bench was a woodern bench. The claws dropped the onions on the bench then went back to the cubard to collect more onions. Several more trips back and forward then the claws switched into the next phase - cutting of the onion. The onions were lined up perfectly on the bench. Three rows, five collums. Fifteen onions. Chef knives snapped out of the claws of the device and started to cut up the onions. Each onion was cut in half, then quarters, and finally sliced. The onions were scopped up and droped into a pan of oil to fry. Once the onions were a lovely gold color they were tiped into a slow cooker. 
+# 'Carrots are ready to go, confirm? Y/n', the device The claws grabbed a orange carrot and dropped it on the bench. The same amount of carrots were used as onions. The knives cut the carrots by a slice lengthways in half, then quarters, and finally slicing. Scopped up by the claws they are dropped in a pan to be fried with oil. Once goldern the carrots are tipped into the slow cooker. Fifteen cups of soft chickpeas are tipped into the pan of oil and fried. They are tipped into the slow cooker. Fifteen medium sized tomatoes travel through the room, being griped by the claw. They are crushed and tipped into the slowcooker.  
+# Five tea spoons of Indian spice mix is tipped into the slowcooker. 
+# 'Coconuts are ready to go, confirm? Y/n', beeped the device in Turbons hand. Enter was hit. The claws moved towards the cubard to retreave the coconuts. Seven medium sized ripe coconuts are dropped on the woodern bench. The knife snaps out of the claw and cuts the coconut clean in half. The claw snaps the knife away and revieles a sharp scoop. The white coconut insides are cut, scooped and tiped into the slowcooker.
+# 'Item ready to cook. Is there anything else?', the device spoke. 
+# The voice was that of Trist Gardens. Her voice was sharp and clear. She spoke with confidence and joy. 
+# 'Fifteen garlic and salt', Turbon requested.
+# 'Garlic are ready to go, confirm Y/n', the device beeped. Turbon slammed the enter key. The claws moved to the cubards and dragged out fifteen cloves of garlic. The knives snapped out and sliced it up finely. Garlic was scooped up and tipped into the slow cooker. 
+# 'Salt is ready to go, conf', before the device had finished beeping Tubon hit the enter key. Salt was tipped into the slow cooker. 
+# 'Item read to cook. Is there anything else?', Gardens voice asked. 
+# 'Confirm', Turbon confirmed. 
+# The claws began mixxing the slowcooker mixture together. Turbon had a late order for a chickpea curry that he was putting though last mintute. 
+# It was a Sunday night and he had a client that wanted a Chickpea Curry for lunch on Monday despritly. Normally he charges extra for weekend food orders but this night he made an exception. This client was special and he knew business would be better in the future because of it. 
+# A raspberry Pi and Audino powered his commucial resturont cooking device. His cellphone was connected to the raspberry pi computer that was connected to the Audino. The laptop was his portable devlopment machine. 
+# Turbon pressed a button a door opened. Server room with racks of servers running. small selfdriving cars moved up and down the hallway. The cars were careful to avoid Turbon as he walled down the hallway. He nodded at each car as they passed by. 
+# The cars had no wheels and moved along the ground by twisting. They moved along with the awkward twist movement. Rubberlike commpletion. Majority of the cars were a yellow and red colour scheme. Several had grayscale colour scheme and traveled in groups of three to five. The grayscale moved slower than the colour scheme and was quick to move out the way when one was heading towards them.
+# Turbons reached a door. It was a large door, double his height. He banged at the door three times.
+# The device in his pocket beeped, 'door status: open'.
+# Turbon turned the handle and opened the door. He stepped into the room. Mid gray covered the room. A dark shape sat in the corner, reading a newspaper. 
+# Turbon paused as his eyes noticed the black shape. he was pulled closer and began walking towards the shape. He stood in front of the shape.
+# 'Chickpea Curry is ready to go, confirm Y/n', Turbons spoke. 
+# A page of the newspaper was flicked over. Turbon waited, frozen.
+# 'Confirm', a sound came from the black shape. The voice was that of Trist Gardens. She still continued to read the newspaper. 
+# Turbons dropped sixteen bags of curries. In each bag were 5 trays of curry. Eighty trays of curry in total. Every tray of curry also came with a tray of rice. Turbon dropped another sixteen bags. In each of these bags were 5 trays of rice.
+# 'Enjoy your curry', Turbons said. 
+# 'Thank you', the voice of Gardens replied. 
+# The dark shape placed the newspaper beside them and 
 # 
+# Charlie MacDonald. Rest in Peace. The grave stone read. Born 24 October 2014. Died 12 December 2093. Trist Gardens stood over the grave. Tears flowing from her eye. It had been two months since MacDonald passed away. She had worked with him for fifteen years at Spike InfoSec. Together they worked on a automatic cooking device. It was used in billions of home and businesses around the world. Spike InfoSec realeased blueprints of the device so anyone with a 3d printer can just print the device. 
 # 
-# 
-# 
+# Drone hovered by MacDonalds head. The claw was attached to a woodern board. MacDonald placed a onion on the board. 
+# 'Cut', he spoke clearly. 
+# 'Confirm', the device beeped back.
+# A knive snapped out of the claw and sliced the onion in half. 
+# A smile rose on MacDonalds face. 
+# 'Jones, get over here and see this'. The door opened and Jones walked towards MacDonald. 
+# 'What is it sir?', Jones asked.
+# 'Watch this Jones', MacDonald said as he placed a onion on the woodern board.
+# 'Cut', he spoke once again. 
+# 'Confirm', the device deeped back. 
+# The knife snapped out. The onion was sliced clean in half.
 # 
 # He had plans to create his own Infomation Security company.                                               
 
@@ -388,11 +426,6 @@ str(opjsnd)
 # Screw the excel file, im just dealing with index page. i am saving achieve in posts folder under urandom 13 character code. 
 
 # <codecell>
-
-
-
-
-
 
 doc = dominate.document(title='Visitor Sign Sheet')
 
