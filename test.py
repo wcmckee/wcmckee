ca3d633fc9198b99d4837583f26d68978627c1e6
wcmckee <will@artcontrol.me>
2014-12-05 21:03:59 +1300

    scp methods

--- test.py
+++ test.py
@@ -11,7 +11,46 @@ import os
 
 # <codecell>
 
-os.listdir('/home')
+lisfir = ('/home/wcmckee/index.xcf')
+
+# <codecell>
+
+lisfir
+
+# <codecell>
+
+def lisfire(localfile, remotehost, remotefile):
+    return os.system('scp "%s" "%s:%s"' % (localfile, remotehost, remotefile) )
+
+def refzem(remotehost, remotefolder):
+    return os.system('scp "%s":"%s" ls -R' % (remotehost, remotefolder))
+
+# <codecell>
+
+lisfire('/home/wcmckee/Documents/getsdrawn-line.png', 'wcmckee@getsdrawn.com', '/home/wcmckee/getsdrawn-line.png')
+
+# <codecell>
+
+checkred = refzem('wcmckee', 'wcmckee.com')
+
+# <rawcell>
+
+
+# <codecell>
+
+checkred.real
+
+# <codecell>
+
+chrez = checkred.conjugate
+
+# <codecell>
+
+checkred.imag
+
+# <codecell>
+
+chrez()
 
 # <codecell>
 
