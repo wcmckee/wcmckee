af4a4331f4ee154e8a712fd18ad54c72eb12b1b2
wcmckee <will@artcontrol.me>
2014-12-15 10:43:12 +1300

    rsync added

--- test.py
+++ test.py
@@ -25,6 +25,19 @@ def lisfire(localfile, remotehost, remotefile):
 def refzem(remotehost, remotefolder):
     return os.system('scp "%s":"%s" ls -R' % (remotehost, remotefolder))
 
+def rsyzmpush(localfile, remotehost, remotefile):
+    return os.system('rsync -azp "%s" "%s:%s"' % (localfile, remotehost, remotefile))
+
+def rsyzmpull(remotehost, remotefile, localfile):
+    return os.system('rsync -azp "%s:%s": "%s"' % (localfile, remotehost, remotefile))
+
+def rsyzmloc(localfile, dilocfil):
+    return os.system('rsync -a "%s" "%s' % (localfile, dilocfil))
+
+# <codecell>
+
+rsyzmloc('/home/wcmckee', '/media/removable/USB Drive/')
+
 # <codecell>
 
 lisfire('/home/wcmckee/Documents/getsdrawn-line.png', 'wcmckee@getsdrawn.com', '/home/wcmckee/getsdrawn-line.png')
